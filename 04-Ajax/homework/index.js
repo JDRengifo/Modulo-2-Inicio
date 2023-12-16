6382b7c314eb87776a008c12d6615aeb4ec43a1e
JDRengifo <josedariorf@gmail.com>
2023-12-16 01:26:48 -0300

    cambios hechos al codigo del m2 ajax

--- 04-Ajax/homework/index.js
+++ 04-Ajax/homework/index.js
@@ -1,60 +1,59 @@
 
-$('#boton').click(function(){
-  $('#lista li').remove();
-    $.get("http://localhost:5000/amigos", (amigo)=>{
-        const listaAmigos = document.querySelector("#lista")
-        amigo.forEach(amistad => {
-          const li = document.createElement("li");
-          li.innerHTML = amistad.name;
-          listaAmigos.appendChild(li)
-      });
-    })
-  });
 
-$('#search').click(function(){
-  const idAmigo = $('#input').val();
-  console.log(idAmigo)
-  $.get("http://localhost:5000/amigos", (amigo)=>{
-    const listaAmigos = document.querySelector("#lista")
-    for(let i= 0; i<amigo.length; i++){
-         if(amigo[i].id == idAmigo){
-            $('#amigo').text(amigo[i].name)
-      }
-    }
+const URL_AMIGOS = "http://localhost:5000/amigos";
+const listaAmigos = document.querySelector("#lista");
+
+const funcionInterna1 = (amigo)=>{
+  amigo.forEach(amistad => {
+    const li = document.createElement("li");
+    li.innerHTML = amistad.name;
+    listaAmigos.appendChild(li)
   })
-})
-
-$('#delete').click(function(){
-   const idAmigo = $('#inputDelete').val();
-  $.get("http://localhost:5000/amigos", (amigo)=>{
-    const listaAmigos = document.querySelector("#lista")
-    for(let i= 0; i<amigo.length; i++){
-      if(amigo[i].id == idAmigo){
-         $.ajax({
-          url: `http://localhost:5000/amigos/${idAmigo}`,
-          type: 'DELETE',
-          success: () => {
-            $('#success').text(`Tu ex-amigo ${amigo[i].name} ha sido eliminado`)
-          }
-        })
-       }
-     }
-   })
-})
-    
-    // let indice = amigo.findIndex(function(persona) {
-    //   return persona.id === idAmigo;
-    // });
-    
-    
-  // console.log(amigo[id].nombre)
-  // const amigoAEliminar = `http://localhost:5000/amigos/${idAmigo}`,
-  // console.log(amigoAEliminar)
-
-
+};
   
-  //   $.ajax({
-    //     type:'DELETE',
-    //     success: (){
-      //     $('#success').text(`${amistad[idAmigo]} ha sido eliminado de la lista`)
-      //   }
+const funcionInterna2 = (amigo)=>{
+  const idAmigo = $('#input').val();
+  for(let i= 0; i<amigo.length; i++){
+    if(amigo[i].id == idAmigo){
+      $('#amigo').text(amigo[i].name)
+      console.log(amigo[i])
+      console.log(amigo[i].id)
+      console.log(i)
+
+    }
+  }
+};
+      
+const funcionInterna3 = (amigo)=>{
+  const idAmigo = $('#inputDelete').val();
+  for(let i= 0; i<amigo.length; i++){
+    if(amigo[i].id == idAmigo){
+      $.ajax({
+        url: `http://localhost:5000/amigos/${idAmigo}`,
+        type: 'DELETE',
+         success: () => {
+          funcionBoton1();
+         $('#success').text(`Tu ex-amigo ${amigo[i].name} ha sido eliminado`)
+         }
+      })
+    }
+  }
+};
+              
+const funcionBoton1 = function(){
+    document.querySelector("img").style.display = "none";
+    $('#lista li').remove();
+    $.get(URL_AMIGOS, funcionInterna1 )};
+                
+const funcionBoton2 = function(){
+  $.get(URL_AMIGOS, funcionInterna2)};
+                  
+const funcionBoton3 = function(){
+  $.get(URL_AMIGOS, funcionInterna3)};
+                    
+$('#boton').click(funcionBoton1);
+                    
+$('#search').click(funcionBoton2)
+                    
+$('#delete').click(funcionBoton3)
+    
